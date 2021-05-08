50a57f6f7d5473aff2a335f5dbcbae7f55b8d897
eledri <eledri1306@gmail.com>
2021-05-08 18:38:26 +0300

    first commit

--- business-logic-layer/order-logic.js
+++ business-logic-layer/order-logic.js
@@ -23,7 +23,7 @@ async function addNewOrderAsync(order, cartId, userId) {
     order.finalPrice,
     order.shippingCity,
     order.shippingStreet,
-    new Date(order.shippingDate).toLocaleDateString("fr-CA"),
+    order.shippingDate,
     new Date().toLocaleDateString("fr-CA"),
     order.creditCard,
   ]);
